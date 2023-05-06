f05ad78799c085e64d00b8a1fb9a100325ff0f74
Siddhanta Poudel <siddhanta.poudel44@gmail.com>
2023-05-06 22:52:44 +0545

    Changes

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -5,8 +5,15 @@ import Logout from "./Logout";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { sendMessageRoute, recieveMessageRoute } from "../utils/APIRoutes";
+import Peer from "peerjs";
 
 export default function ChatContainer({ currentChat, socket }) {
+  const [peerId, setPeerId] = useState("");
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+  const remoteVideoRef = useRef(null);
+  const currentUserVideoRef = useRef(null);
+  const peerInstance = useRef(null);
+
   const [messages, setMessages] = useState([]);
   const scrollRef = useRef();
   const [arrivalMessage, setArrivalMessage] = useState(null);
@@ -22,7 +29,50 @@ export default function ChatContainer({ currentChat, socket }) {
     setMessages(response.data);
   }, [currentChat]);
 
+  const call = (remotePeerId) => {
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    getUserMedia({ video: true, audio: true }, (mediaStream) => {
+      currentUserVideoRef.current.srcObject = mediaStream;
+      currentUserVideoRef.current.play();
+
+      const call = peerInstance.current.call(remotePeerId, mediaStream);
+
+      call.on("stream", (remoteStream) => {
+        remoteVideoRef.current.srcObject = remoteStream;
+        remoteVideoRef.current.play();
+      });
+    });
+  };
+
   useEffect(() => {
+    const peer = new Peer();
+
+    peer.on("open", (id) => {
+      setPeerId(id);
+    });
+
+    peer.on("call", (call) => {
+      var getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+
+      getUserMedia({ video: true, audio: true }, (mediaStream) => {
+        currentUserVideoRef.current.srcObject = mediaStream;
+        currentUserVideoRef.current.play();
+        call.answer(mediaStream);
+        call.on("stream", function (remoteStream) {
+          remoteVideoRef.current.srcObject = remoteStream;
+          remoteVideoRef.current.play();
+        });
+      });
+    });
+
+    peerInstance.current = peer;
     const getCurrentChat = async () => {
       if (currentChat) {
         await JSON.parse(
@@ -32,6 +82,29 @@ export default function ChatContainer({ currentChat, socket }) {
     };
     getCurrentChat();
   }, [currentChat]);
+
+  const checkValidJSON = (text) => {
+    try {
+      JSON.parse(text);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const initiateCall = () => {
+    handleSendMsg(JSON.stringify({ userId: peerId }));
+    setRemotePeerIdValue(peerId);
+    call(peerId);
+  };
+
+  const joinCall = (message) => {
+    console.log(message);
+    let data = JSON.parse(message);
+    setRemotePeerIdValue(data.userId);
+    call(data.userId);
+    console.log(data.userId);
+  };
 
   const handleSendMsg = async (msg) => {
     const data = await JSON.parse(
@@ -71,31 +144,50 @@ export default function ChatContainer({ currentChat, socket }) {
 
   return (
     <Container>
-      <div className="chat-header">
-        <div className="user-details">
-          <div className="avatar">
+      <div className='chat-header'>
+        <div className='user-details'>
+          <div className='avatar'>
             <img
               src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
-              alt=""
+              alt=''
             />
           </div>
-          <div className="username">
+          <div className='username'>
             <h3>{currentChat.username}</h3>
           </div>
         </div>
-        <button> Call</button>
+        <button onClick={initiateCall}> Call</button>
         <Logout />
       </div>
-      <div className="chat-messages">
+      <div className='chat-messages'>
+        <div className='chat-video-mode'>
+        <video ref={currentUserVideoRef} />
+        <video ref={remoteVideoRef} />
+        </div>
         {messages.map((message) => {
-          return (
+          return checkValidJSON(message.message) ? (
+            <div
+              className={`message ${message.fromSelf ? "sended" : "recieved"}`}
+            >
+              <div
+                onClick={() => {
+                  joinCall(message.message);
+                }}
+                className={`content ${
+                  message.fromSelf ? "clickToCallHide" : "clickToCall"
+                }`}
+              >
+                <p>Click here to join call</p>
+              </div>
+            </div>
+          ) : (
             <div ref={scrollRef} key={uuidv4()}>
               <div
                 className={`message ${
                   message.fromSelf ? "sended" : "recieved"
                 }`}
               >
-                <div className="content ">
+                <div className='content '>
                   <p>{message.message}</p>
                 </div>
               </div>
@@ -103,6 +195,7 @@ export default function ChatContainer({ currentChat, socket }) {
           );
         })}
       </div>
+
       <ChatInput handleSendMsg={handleSendMsg} />
     </Container>
   );
@@ -137,7 +230,22 @@ const Container = styled.div`
       }
     }
   }
+  .chat-video-mode {
+    position: sticky;
+    top:0;
+    margin-left: -20px
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .chat-video-mode video {
+    margin-bottom: 13px;
+    height: calc(50%);
+    object-fit: fill;
+  }
   .chat-messages {
+    position: relative;
     padding: 1rem 2rem;
     display: flex;
     flex-direction: column;
@@ -165,6 +273,12 @@ const Container = styled.div`
           max-width: 70%;
         }
       }
+    }
+    .clickToCall {
+      cursor: pointer;
+    }
+    .clickToCallHide {
+      display: none;
     }
     .sended {
       justify-content: flex-end;
